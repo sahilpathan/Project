061344ec203934ef0a0ae99834663e1ed354f7b2
shore <pathan.sahil09@gmail.com>
2017-02-12 17:20:40 +0530

    Add files via upload

---
+++ src/coll.java
@@ -0,0 +1,20 @@
+
+import com.mongodb.DBCursor;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Sahil
+ */
+class coll {
+
+    static DBCursor find() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
